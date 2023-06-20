def1c9b8acbe886d56be6ebec07a7edd9fe0468b
Jörg Finkeisen <joerg@finkeisen.net>
2023-06-20 18:34:48 +0200

    display weather

--- app.js
+++ app.js
@@ -6,16 +6,19 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  const url = "https://api.openweathermap.org/data/2.5/weather?q=London,UK&appid=6691763b26f6cae92f9e7f9673a5fbe2"
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=Berlin,DE&appid=6691763b26f6cae92f9e7f9673a5fbe2&units=metric"
   https.get(url, (response) => {
     console.log(response.statusCode);
   
     response.on('data', (data) => {
       const weatherData = JSON.parse(data);
-      console.log(weatherData);
+      const temp = weatherData.main.temp;
+      const clouds = weatherData.weather[0].description;
+      res.write('<h1>The temperature in Berlin is ' + temp + '°C.</h1>');
+      res.write('<p>The cloud situation is ' + clouds + '.</p>')
     })
+
   })
-  res.send('Server is up and running');
 })
 
 app.listen(port, () => {
